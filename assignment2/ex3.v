e6b299981bf5417d6e0a41eceeb558059b709c4a
Mikolaj Kowalik <kowalikmikolaj95@gmail.com>
2019-10-22 20:57:26 +0200

    Add [2] ex3.v

--- assignment2/ex3.v
+++ assignment2/ex3.v
@@ -5,20 +5,17 @@ endmodule
 
 module encX(output [2**(i_len-1)-1:0] o, output [0:0] v, input [2**(i_len)-1:0] in);
 	parameter i_len = 3;
-	wire [2**(i_len-1)-1:0] eo, ev, av;
+	wire [2**(i_len-1)-1:0] eo, ev;
 	genvar i;
-	genvar j;
 	generate
 		for (i = 0; i < 2**(i_len-1); i = i + 1) begin : gen_enc
 			enc2 e(eo[i], ev[i], in[i*2+1:i*2]);
-			if (i == 0)
-				or or1(av[i], ev[i], ev[i]);
-			else
-				and and1(av[i], ev[i], ev[i-1]);
 		end
 	endgenerate
 
-	assign o = 6;
+	wire [2**(i_len-1)-1:0] log = ($clog2(ev&-ev));
+	wire [2**(i_len-1)-1:0] log2 = log << 1;
+	assign o = log2 + eo[log];
 
 	assign v = |ev;
 endmodule
@@ -31,14 +28,11 @@ module testbench;
 	encX #(.i_len(word_len)) e(out, v, in);
 	initial begin
 		in = 0;
-		repeat(2**(word_len)) begin
+		repeat(2**(2**(word_len))) begin
 			#1;
 			$display("in=%b v=%b, o=%d", in, v, out);
 			in = in + 1;
 		end
-			in = 0;
-			#1;
-			$display("in=%b v=%b, o=%d", in, v, out);
 		$finish;
 	end
 endmodule
